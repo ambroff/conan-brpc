0711f8c376252b85732204fc302cc32a62cdd601
Kyle Ambroff-Kao <kambroff@linkedin.com>
2022-11-17 21:17:08 -0800

    Package brpc 1.3.0

--- conanfile.py
+++ conanfile.py
@@ -1,36 +1,68 @@
 from conans import CMake, ConanFile, tools
 import os
 
+
 class BrpcConan(ConanFile):
     name = "brpc"
-    version = "0.9.5"
-    license = "https://github.com/brpc/brpc/blob/master/LICENSE"
-    url = "https://github.com/ambroff/conan-brpc"
-    description = "A library that provides an embeddable, persistent key-value store for fast storage."
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    generators = "cmake"
+    version = '1.3.0'
+    license = "https://github.com/apache/incubator-brpc/blob/1.3.0/LICENSE"
+    url = "https://github.com/jjkoshy/conan-recipes/conan-brpc"
+    description = "An industrial-grade RPC framework used throughout Baidu"
+    settings = "os", "os_build", "compiler", "build_type", "arch", "arch_build"
+    options = {
+            "shared": [True, False],
+            # see the leveldb recipe in conan-leveldb/conanfile.py for why we
+            # need this
+            "with_snappy": [True, False] }
+    default_options = {
+            "shared": False,
+            "with_snappy": False }
+    # cmake_paths generates a file which we set as the cmake toolchain file
+    generators = ('cmake_paths', 'CMakeDeps')
+    exports_sources = ["CMakeLists.txt", "patches/*"]
 
-    source_tgz = 'https://github.com/brpc/brpc/archive/%s.tar.gz' % version
-    checksum = 'e409994fb1fa191e9c34218d1d87d5fa375354acfe1448037a7dbf1b27fd1d6b'
+    requires = (
+        'gflags/2.2.2',
+        'protobuf/3.21.4',
+        'leveldb/1.23',
+        'openssl/1.1.1s',)
 
-    requires = ()
-
-    def source(self):
-        self.output.info("Downloading %s" %self.source_tgz)
-        tools.get(self.source_tgz, sha256=self.checksum)
+    def config(self):
+        # must build protobuf with zlib since brpc build does not make it optional
+        self.options['protobuf'].with_zlib = True
+        self.options['leveldb'].with_snappy = self.options.with_snappy
 
     @property
-    def subfolder(self):
-        return "brpc-%s" % self.version
+    def zip_folder_name(self):
+        return "incubator-%s-%s" % (self.name, self.version)
+
+    def source(self):
+        zip_name = "%s.zip" % self.version
+        tools.download("https://github.com/apache/incubator-brpc/archive/%s.zip" % self.version, zip_name)
+        tools.unzip(zip_name)
+        # TODO: move to conandata.yml
+        tools.check_md5(zip_name, "0b2e4af0a9b61705d73a7a1957d9cd4f")
+        os.unlink(zip_name)
+        with tools.chdir(self.zip_folder_name):
+            # TODO: switch to conandata.yml approach
+            tools.patch(patch_file="../patches/brpc-1.3.0.patch", strip=1)
+
+            # # add snappy to list of libs to link with if with_snappy is true
+            # if self.options.with_snappy:
+            #     tools.patch(patch_file="../patches/snappy.patch", strip=1)
 
     def configure_cmake(self):
         cmake = CMake(self)
+
+        cmake.definitions['CMAKE_TOOLCHAIN_FILE'] = 'conan_paths.cmake'
+        # TODO: the source_subfolder should be something generic like
+        # src or source-subfolder, and we should rename. change
+        # cmakelists.txt also
         cmake.configure(
-            source_folder='brpc-{}'.format(self.version),
             defs={
                 'CMAKE_POSITION_INDEPENDENT_CODE': True,
+                'protobuf_MODULE_COMPATIBLE': True,
+                'BUILD_BRPC_TOOLS': False,
             })
         return cmake
     
@@ -39,7 +71,7 @@ class BrpcConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE", src=self.subfolder, keep_path=False)
+        self.copy("LICENSE", "incubator-brpc-0.9.6", keep_path=False)
         cmake = self.configure_cmake()
         cmake.install()
 
@@ -47,3 +79,4 @@ class BrpcConan(ConanFile):
         self.cpp_info.libs = ["brpc"]
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("pthread")
+
